657de8c5765214fc060e5aebcdb8ab8735dc401a
Aryan Jain <aryan.ajtdl156@gmail.com>
2025-05-15 13:41:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
+  origin: '*',
+  credentials: false 
 }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
